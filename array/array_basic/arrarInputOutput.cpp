d32ef46f194085a4005650007f4088effa93c3a2
kamlesh802207 <145148640+kamlesh802207@users.noreply.github.com>
2024-03-27 02:03:33 +0530

    update

--- array/array_basic/arrarInputOutput.cpp
+++ array/array_basic/arrarInputOutput.cpp
@@ -1,15 +1,19 @@
+//Input output in array
 #include<iostream>
 using namespace std;
 int main()
 { 
     int n;
-    cout<<"Enter the size of array"<<endl;
+    cout<<endl;
+    cout<<"Enter the size of array=";
     cin>>n;
     int arr[n];
+    cout<<"Enter array element..."<<endl;
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
+    cout<<"Array = ";
     for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
