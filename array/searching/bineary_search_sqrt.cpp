b4e0e2f1f3ac7f1588350cb85edb587e0079ab63
kamleshvvit <kamleshkumar802207@gmail.com>
2024-04-04 23:58:57 +0530

    initial commit

--- array/searching/bineary_search_sqrt.cpp
+++ array/searching/bineary_search_sqrt.cpp
@@ -1,15 +1,15 @@
-/*Given a non-negative integer x, return the square root of x 
-rounded down to the nearest integer. The returned integer 
-should be non-negative as well.
+  /*Given a non-negative integer x, return the square root of x 
+  rounded down to the nearest integer. The returned integer 
+  should be non-negative as well.
 
-Example 1:
+  Example 1:
        Input: x = 4
        Output: 2
 
-Example 2:
+  Example 2:
        Input: x = 8
        Output: 2
-*/
+ */
 #include<iostream>
 using namespace std;
 int sqrt(int num)
---
+++ array/searching/bs_mountain.cpp
@@ -0,0 +1,48 @@
+/*An array arr is a mountain if the following properties hold:
+
+Example 1:
+  Input: arr = [0,1,0]
+  Output: 1
+
+Example 2:
+  Input: arr = [0,2,1,0]
+  Output: 1
+
+Example 3:
+  Input: arr = [0,10,5,2]
+  Output: 1*/
+
+  #include<iostream>
+  using namespace std;
+  int arr_mountain(int arr[],int n)
+  {
+    int start=0,end=n,mid;
+    while(start<=end)
+    {
+      mid=start+(end-start)/2;
+      if(arr[mid]>arr[mid-1] && arr[mid]>mid+1)
+      {
+      return mid;
+      }
+      else if(arr[mid]>arr[mid-1])
+      start=mid+1;
+      else
+      end=mid-1;
+    }
+    return -1;
+    
+  }
+  int main()
+  {
+    int arr[100],n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    cout<<endl;
+    cout<<"Enter array  element:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Peak index in mountain array:"<<arr_mountain(arr,n);
+    return 0;
+  }
